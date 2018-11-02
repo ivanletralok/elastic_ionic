0b0d5bf288b23408a551d5ae3ae7b58b0e89b75e
ivanletralok <ivanletralok@gmail.com>
2018-11-02 11:50:36 -0500

    Creo que esta listo! agrengando boton buscar y toast para mostrar

--- src/pages/frutas/frutas.ts
+++ src/pages/frutas/frutas.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import elastic from '../home/elastic.js';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the FrutasPage page.
  *
@@ -15,15 +17,16 @@ import elastic from '../home/elastic.js';
   templateUrl: 'frutas.html',
 })
 export class FrutasPage {
+  buscarF;
   t = 0;
   result: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
 
   ionViewDidLoad() {
-     this.mostrar();
+    this.mostrar();
   }
 
   mostrar() {
@@ -31,11 +34,38 @@ export class FrutasPage {
     elastic.search("*").then(function (results) {
       self.t = results.hits.hits.length;
       self.result = results.hits.hits;
-      console.log(self.result)
+      // console.log(self.result)
       // for (let r of self.result)
       //   console.log(r);
     });
   }
 
+  buscar() {
+    let self = this;
+    let cont = 0;
+    elastic.search(self.buscarF).then(function (results) {
+      let it = results.hits.hits.length;
+      for (let i = 0; i < it; i++) {
+        cont++;
+      }
+      if (cont == 0 ) {
+        const toast = self.toastCtrl.create({
+          message: 'No Encontrado',
+          duration: 3000
+        });
+        toast.present();
+      }else{
+        const toast = self.toastCtrl.create({
+          message: 'Encontrada!,  \n'
+          + ' La Fruta Buscada es : ' + self.buscarF,
+          duration: 3000
+        });
+        toast.present();
+        self.buscarF = "";
+      }
+
+    });
+
+  }
 
 }
